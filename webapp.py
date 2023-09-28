a35bbbacc8812a6665c898c8ea619013615ffa77
Arpan Swain <kankalase07@gmail.com>
2023-09-28 09:05:56 +0530

    Initial Commit

---
+++ webapp.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+def add_todo():
+    todo_local = st.session_state["new_todo"]
+    global todos
+    if todo_local not in todos:
+        todos.append(todo_local + "\n")
+        functions.write_todos(todos)
+
+st.title("My Todo App")
+
+
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key = todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        # rerun required
+        st.experimental_rerun()
+
+
+
+st.text_input(label="",placeholder="add a todo here",
+              on_change=add_todo,key="new_todo")
